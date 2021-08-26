fb5daf5f615acc9017fa26a22043964d7bd267c0
Nouamane Lachhab <nouamanelachhab@outlook.com>
2021-08-26 16:35:06 +0100

    Responsive Slider

--- app/src/main/java/me/inventory/admin/augscan/FirstSlides.java
+++ app/src/main/java/me/inventory/admin/augscan/FirstSlides.java
@@ -7,9 +7,11 @@ import android.app.StatusBarManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Html;
 import android.view.View;
 import android.view.Window;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.material.appbar.AppBarLayout;
 
@@ -17,17 +19,20 @@ public class FirstSlides extends AppCompatActivity {
     private ViewPager vpg;
     private LinearLayout la;
     private SliderAdapter sliderAdapter;
-
+    private TextView[] mDots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_slides);
         getSupportActionBar().hide();
+        la = findViewById(R.id.dotsLayout);
 
         vpg = findViewById(R.id.vpg);
         sliderAdapter = new SliderAdapter(this);
         vpg.setAdapter(sliderAdapter);
+        addDotsIndicator(0);
+        vpg.addOnPageChangeListener(viewListener);
 
 
     }
@@ -36,4 +41,41 @@ public class FirstSlides extends AppCompatActivity {
         Intent intent1 = new Intent(FirstSlides.this, Home.class);
         startActivity(intent1);
     }
+
+    public void addDotsIndicator(int position){
+        mDots = new TextView[3];
+        la.removeAllViews();
+        for (int i=0;i< mDots.length; i++)
+        {
+
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+            mDots[i].setTextSize(40);
+            mDots[i].setTextColor(getResources().getColor(R.color.transparentwhite));
+            la.addView(mDots[i]);
+
+        }
+        if(mDots.length > 0){
+            mDots[position].setTextColor(getResources().getColor(R.color.white));
+        }
+
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDotsIndicator(position);
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
