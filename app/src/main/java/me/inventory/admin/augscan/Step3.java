58c75190d43f1da0778adb04698e186e6cfdbf21
Nouamane Lachhab <nouamanelachhab@outlook.com>
2021-08-28 14:09:13 +0100

    Password Validation

--- app/src/main/java/me/inventory/admin/augscan/Step3.java
+++ app/src/main/java/me/inventory/admin/augscan/Step3.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -18,19 +19,19 @@ import android.widget.Toast;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class Step2 extends AppCompatActivity {
+public class Step3 extends AppCompatActivity {
     private ImageView progressBar;
     private TextView textView;
     TextInputEditText input1;
+    TextInputEditText input2;
     ConstraintLayout button3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_step2);
+        setContentView(R.layout.activity_step3);
         getSupportActionBar().hide();
     }
 
@@ -41,9 +42,6 @@ public class Step2 extends AppCompatActivity {
         startActivity(viewIntent);
 
     }
-
-
-
 
     public static int getRandom(int from, int to) {
         if (from < to)
@@ -68,34 +66,38 @@ public class Step2 extends AppCompatActivity {
         rotateAnimation.setRepeatCount(Animation.INFINITE);
         progressBar.setAnimation(rotateAnimation);
         input1 = findViewById(R.id.input1);
-
-        String email = input1.getText().toString().trim();
-
-        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
-
-
+        input2 = findViewById(R.id.input2);
 
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                if (!input1.getText().toString().matches("")) {
+                    if (input1.getText().toString().equals(input2.getText().toString())) {
+                        Intent homeIntent = new Intent(Step3.this, Home.class);
+                        startActivity(homeIntent);
+                        finish();
+                    }
 
+                    else {
+                        input1.setError("Password Mismatch");
+                        input2.setError("Password Mismatch");
+                        textView.setText("CONTINUE");
+                        progressBar.setVisibility(View.GONE);
+                        progressBar.clearAnimation();
 
-                if(email.matches(emailPattern)){
-                    Intent homeIntent = new Intent(Step2.this,Step3.class);
-                    startActivity(homeIntent);
-                    finish();
+                    }
                 }
                 else {
-
-                    input1.setError("Invalid Email!");
+                    input1.setError("Enter a Password");
                     textView.setText("CONTINUE");
                     progressBar.setVisibility(View.GONE);
                     progressBar.clearAnimation();
+
                 }
+
 
             }
         },getRandom(500,4000));
     }
-
 }
